293f213a70aca10e053f117e2d156381f476ba53
divergent3 <sergejsabala@gmail.com>
2020-08-21 00:18:00 +0300

    Добавьте файлы проекта.

---
+++ FiveOnFive/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiveOnFive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // просим пользователя ввести число с окончанием 5.
+            Console.WriteLine("Введите число заканчивающееся на 5");
+            string nam = Console.ReadLine();
+            string s = nam;
+            //берем длину строки и отнимаем последний символ.
+            int nam2 = nam.Length - 1;
+            nam = nam.Remove(nam2);
+            //конвентируем в инт и добавляем 1.
+            int Sum=Convert.ToInt32(nam)+1;
+            //умножаем полученный результат на число пользователя.
+            int Sqrt =Convert.ToInt32(nam) * Sum;
+            //конвектируем полученное число в строку и добавляем 25.
+            String Sum2 = Convert.ToString(Sqrt) +"25";
+            Console.WriteLine(s +" " +" "+ "в квадрате" + "=" + Sum2);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FiveOnFive/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("FiveOnFive")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FiveOnFive")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("7c803643-7f0d-41d6-bcce-54ed6e683b5e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
